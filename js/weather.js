7d2aed707cd39b48de065876ea06e2f0a15c07ca
Project <48446774+project11x@users.noreply.github.com>
2021-03-17 14:07:31 +0100

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '76a81da95e483737bb1404f0e841dd52';
 
 // Set Position function
 setPosition();
